62c48b9907e244c7b6d659acf2d415edc96c6b7f
DanRan1910 <danielalvesdasilva453@gmail.com>
2024-08-02 20:17:45 -0300

    Script relacionando estrangeiras

--- dbPets.sql
+++ dbPets.sql
@@ -7,21 +7,26 @@ use dbPets;
 -- Visualizando banco de dados
 show databases;
 -- Criando tabelas no Banco de dados
-create table tbUsuarios(
-codUsu int not null auto_increment,
-nome varchar (50) not null,
-senha varchar (10) not null,
-primary key (codUsu)
-);
+
 create table tbFuncionarios(
 codFunc int not null auto_increment,
 nome varchar (100) not null,
 email varchar (100),
-telCel char(10))
-primary key (codFunc)
+telCel char(10),
+cpf char (14),
+primary key(codFunc)
 );
 
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar (50) not null,
+senha varchar (10) not null,
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc)references tbFuncionarios(codFunc)
+);
 -- visualiando tabelas
 show tables;
 -- visualisando a estrutura das tabelas
-desc tbUsuarios
+desc tbUsuarios;
+desc tbFuncionarios;
