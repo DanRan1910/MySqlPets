4a675f2b3ea5a9bd5e529815958d9226292f9f66
DanRan1910 <danielalvesdasilva453@gmail.com>
2024-08-02 19:50:23 -0300

    Script.02 adiconando table

--- dbPets.sql
+++ dbPets.sql
@@ -6,3 +6,22 @@ create database dbPets;
 use dbPets;
 -- Visualizando banco de dados
 show databases;
+-- Criando tabelas no Banco de dados
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar (50) not null,
+senha varchar (10) not null,
+primary key (codUsu)
+);
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar (100) not null,
+email varchar (100),
+telCel char(10))
+primary key (codFunc)
+);
+
+-- visualiando tabelas
+show tables;
+-- visualisando a estrutura das tabelas
+desc tbUsuarios
